69b6359e3989be729df9d74e0fbbb0e1a07613e7
Adil Hassan <adil27jan@outlook.com>
2022-08-13 12:38:36 +0400

    inprogress

---
+++ TMSAPI/TMSAPI.api/Data/XAMControlX4Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMSAPI.api.Models.Domain;
+
+namespace TMSAPI.api.Data
+{
+    public class XAMControlX4Context:DbContext
+    {
+        public XAMControlX4Context(DbContextOptions<XAMControlX4Context> options):base(options)
+        {
+
+        }
+        public DbSet<X4AlarmConfigs> X4AlarmConfigs { get; set; }
+
+        public DbSet<X4AlarmGroups> X4AlarmGroups { get; set; }
+        public DbSet<X4InstanceRemarks> X4InstanceRemarks { get; set; }
+    }
+}
---
+++ TMSAPI/TMSAPI.api/Data/XAMRuntimeX4Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMSAPI.api.Models.Domain;
+
+namespace TMSAPI.api.Data
+{
+    public class XAMRuntimeX4Context:DbContext
+    {
+        public XAMRuntimeX4Context(DbContextOptions <XAMRuntimeX4Context> options) : base(options)
+        {
+
+        }
+        public DbSet<X4Journals> X4Journals { get; set; }
+    }
+}
